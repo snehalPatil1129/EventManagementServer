8a765fe2f4cf38ed6907f067fcd8027254c1f704
Snehal Patil <snehal.patil@Eternus.local>
2018-07-05 15:41:48 +0530

    Registration response CRUD

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const event = require('./routes/event');
 const session = require('./routes/session');
 const userProfile = require('./routes/userProfile');
 
+const registration = require('./routes/registrationResponse')
 mongoose.connect('mongodb://snehal.patil:espl123@ds227171.mlab.com:27171/eventmanagementapp')
 .then(() => console.log('Connected to MongoDB...'))
 .catch(err => console.error('Could not connect to MongoDB...'));
@@ -22,7 +23,7 @@ app.use('/api/authenticate', authenticate);
 app.use('/api/event',event);
 app.use('/api/session',session);
 app.use('/api/userProfile',userProfile);
-
+app.use('/api/registration', registration);
 //app.use(error);  //central error handling using express middleware
 
 
