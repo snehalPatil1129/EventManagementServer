cdbbea7833775087fef3b0f256f42619600f680e
Snehal Kale <snehal.kale@eternussolutions.com>
2018-07-20 17:22:09 +0530

    added eventId in userProfile module

--- models/userProfile.js
+++ models/userProfile.js
@@ -2,16 +2,21 @@ const mongoose = require('mongoose');
 const Joi = require('joi');
 
 const UserProfiles = mongoose.model('UserProfiles', new mongoose.Schema({
-     profileName :{
-         type : String,
-         rerquired : true
-     }
+    profileName: {
+        type: String,
+        rerquired: true
+    },
+    eventId: {
+        type: String,
+        rerquired: true
+    }
 }))
 
-function validateProfile(userprofiles){
-   const schema = {
-       profileName : Joi.string().required()
-   }
+function validateProfile(userprofiles) {
+    const schema = {
+        profileName: Joi.string().required(),
+        eventId: Joi.string().required(),
+    }
     return Joi.validate(userprofiles, schema);
 }
 
