bb11eda89d84fa544d3dc1703743d27df984eccc
Snehal Patil <snehal.patil@Eternus.local>
2018-07-26 15:27:28 +0530

    sponsor api created

--- routes/sponsor.js
+++ routes/sponsor.js
@@ -7,7 +7,7 @@ const { Sponsors, validateSponsor } = require('../models/sponsor');
 
 router.get('/', async (req, res) => {
     try {
-        const sponsors = await Sponsors.find();
+        const sponsors = await Sponsors.find().populate('event');;
         res.send(sponsors);
     } catch (error) {
         res.send(error.message);
@@ -55,6 +55,17 @@ router.get('/:id', async (req, res) => {
         res.send(error.message);
     }
 
+})
+
+router.get('/event/:id', async (req, res) => {
+    try {
+        const sponsor = await Sponsors.find().where('event').equals(req.params.id).populate('event');
+        if (!sponsor) return res.status(404).send('The Sponsors for given event ID was not found.');
+        res.send(sponsor);
+    }
+    catch (error) {
+        res.send(error.message);
+    }
 })
 
 router.delete('/:id', async (req, res) => {
