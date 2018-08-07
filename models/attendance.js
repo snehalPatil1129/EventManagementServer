17ee8d549ffa9aa07180fe6f762209335f673686
Snehal Kale <snehal.kale@eternussolutions.com>
2018-08-07 15:33:55 +0530

    solved eslint problems in all models

--- models/attendance.js
+++ models/attendance.js
@@ -1,38 +1,41 @@
-const mongoose = require('mongoose');
-const Joi = require('joi');
+const mongoose = require("mongoose");
+const Joi = require("joi");
 
-const Attendance = mongoose.model('Attendance', new mongoose.Schema({
-    event : {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Events'
+const Attendance = mongoose.model(
+  "Attendance",
+  new mongoose.Schema({
+    event: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Events"
     },
-    session : {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Sessions'
+    session: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Sessions"
     },
-    attendee : {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Attendee'
+    attendee: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Attendee"
     },
-    scannedBy : {
-        type : String,
-        required : true
+    scannedBy: {
+      type: String,
+      required: true
     },
-    time :{
-        type : String,
-        required : true
+    time: {
+      type: String,
+      required: true
     }
-}));
+  })
+);
 
 function validateAttendance(attendance) {
-    const schema = {
-        event : Joi.required(),
-        session : Joi.required(),
-        attendee : Joi.required(),
-        scannedBy : Joi.string().required(),
-        time : Joi.string().required(),
-    };
-    return Joi.validate(attendance, schema);
+  const schema = {
+    event: Joi.required(),
+    session: Joi.required(),
+    attendee: Joi.required(),
+    scannedBy: Joi.string().required(),
+    time: Joi.string().required()
+  };
+  return Joi.validate(attendance, schema);
 }
 exports.Attendance = Attendance;
 exports.validateAttendance = validateAttendance;
